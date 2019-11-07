bf364329ecba01777b86b1f0a7bf2373520a020f
oshimaharuna <ooooooshim115@gmail.com>
2019-11-07 21:25:27 +0900

    add FailureObject.cs and fix TurnCorner.cs

---
+++ Assets/Scripts/Tutorial/FailureObject.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FailureObject : MonoBehaviour
+{
+    void OnTriggerEnter(Collider col)
+    {
+        //if the player hits one obstacle, it's game over
+        if (col.gameObject.tag == Constants.PlayerTag)
+        {
+            Debug.Log("@@@hit" + TurnCorner.Instance.hitFailureObject);
+            TurnCorner.Instance.hitFailureObject = true;
+        }
+    }
+}
